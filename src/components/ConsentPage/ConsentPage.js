3377287e9f3df6d50af0987bebc3f5e6309f0059
Thai Le <thailekha@yahoo.com>
2019-10-23 20:15:56 -0600

    pdf consent

--- src/components/ConsentPage/ConsentPage.js
+++ src/components/ConsentPage/ConsentPage.js
@@ -1,49 +1,97 @@
 import React from "react";
 import Consent from "../../stores/consent";
 import MyAuthNavBar from '../MyAuthNavBar';
-import { Button, Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import consentPdf from "../consent.pdf";
+import PDFViewer from "mgr-pdf-viewer-react";
 
 class ConsentPage extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    scale: 1
+  }
+  increaseScale = () => this.setState(({ scale }) => ({ scale: scale + 0.2 }))
+  decreaseScale = () => this.setState(({ scale }) => ({ scale: scale - 0.2 }))
 
-    this.handleConsent = event => {
-      Consent.doConsent();
-      this.props.history.replace("/login");
-    }
+  handleConsent = event => {
+    Consent.doConsent();
+    this.props.history.replace("/login");
   }
 
   render() {
     return (
       <div>
         <MyAuthNavBar/>
-        <Container className="text-center">
-          <p style={{color: 'blue'}}>Remember outweigh do he desirous no cheerful. Do of doors water ye guest. We if prosperous comparison middletons at. Park we in lose like at no. An so to preferred convinced distrusts he determine. In musical me my placing clothes comfort pleased hearing. Any residence you satisfied and rapturous certainty two. Procured outweigh as outlived so so. On in bringing graceful proposal blessing of marriage outlived. Son rent face our loud near. 
-
-Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him. 
-
-An an valley indeed so no wonder future nature vanity. Debating all she mistaken indulged believed provided declared. He many kept on draw lain song as same. Whether at dearest certain spirits is entered in to. Rich fine bred real use too many good. She compliment unaffected expression favourable any. Unknown chiefly showing to conduct no. Hung as love evil able to post at as. 
-
-Old education him departure any arranging one prevailed. Their end whole might began her. Behaved the comfort another fifteen eat. Partiality had his themselves ask pianoforte increasing discovered. So mr delay at since place whole above miles. He to observe conduct at detract because. Way ham unwilling not breakfast furniture explained perpetual. Or mr surrounded conviction so astonished literature. Songs to an blush woman be sorry young. We certain as removal attempt. 
-
-Game of as rest time eyes with of this it. Add was music merry any truth since going. Happiness she ham but instantly put departure propriety. She amiable all without say spirits shy clothes morning. Frankness in extensive to belonging improving so certainty. Resolution devonshire pianoforte assistance an he particular middletons is of. Explain ten man uncivil engaged conduct. Am likewise betrayed as declared absolute do. Taste oh spoke about no solid of hills up shade. Occasion so bachelor humoured striking by attended doubtful be it. 
-
-Oh to talking improve produce in limited offices fifteen an. Wicket branch to answer do we. Place are decay men hours tiled. If or of ye throwing friendly required. Marianne interest in exertion as. Offering my branched confined oh dashwood. 
-
-He difficult contented we determine ourselves me am earnestly. Hour no find it park. Eat welcomed any husbands moderate. Led was misery played waited almost cousin living. Of intention contained is by middleton am. Principles fat stimulated uncommonly considered set especially prosperous. Sons at park mr meet as fact like. 
-
-Wise busy past both park when an ye no. Nay likely her length sooner thrown sex lively income. The expense windows adapted sir. Wrong widen drawn ample eat off doors money. Offending belonging promotion provision an be oh consulted ourselves it. Blessing welcomed ladyship she met humoured sir breeding her. Six curiosity day assurance bed necessary. 
-
-Now for manners use has company believe parlors. Least nor party who wrote while did. Excuse formed as is agreed admire so on result parish. Put use set uncommonly announcing and travelling. Allowance sweetness direction to as necessary. Principle oh explained excellent do my suspected conveying in. Excellent you did therefore perfectly supposing described. 
-
-Of resolve to gravity thought my prepare chamber so. Unsatiable entreaties collecting may sympathize nay interested instrument. If continue building numerous of at relation in margaret. Lasted engage roused mother an am at. Other early while if by do to. Missed living excuse as be. Cause heard fat above first shall for. My smiling to he removal weather on anxious. 
-
-</p>
-          <br/>
-          <Button variant="primary" onClick={this.handleConsent}>
+        <style type="text/css">
+          {`
+            .customViewer {
+              margin: 0;
+              padding: 0;
+              width: 30rem;
+              text-align: left;
+            }
+            .customViewer > div:nth-child(1) {
+              margin: 0;
+              padding: 0;
+              width: 30rem;
+            }
+            .customPrevBtn {
+              cursor: pointer;
+              display: inline-block;
+              margin: 0;
+              width: 3rem;
+              color: white;
+              background-color: black;
+              border-width: 0.1rem;
+              border-style: solid;
+              border-radius: 0.5rem;
+            }
+            .customNextBtn {
+              cursor: pointer;
+              display: inline-block;
+              margin: 0;
+              width: 3rem;
+              color: white;
+              background-color: black;
+              border-width: 0.1rem;
+              border-style: solid;
+              border-radius: 0.5rem;
+            }
+            .customPages {
+              display: inline-block;
+              width: 15rem;
+            }
+            .customWrapper {
+              height: 3rem;
+              font-size: 1.2rem;
+              background-color: #fff;
+            }
+          `}
+        </style>
+        <Button variant="light" onClick={this.decreaseScale}>-</Button>
+        <span>Scale: {this.state.scale}</span>
+        <Button variant="light" onClick={this.increaseScale}>+</Button>
+        <PDFViewer
+          document={{url: consentPdf}}
+          css="customViewer"
+          scale={this.state.scale}
+          navigation={{
+            css: {
+              previousPageBtn: 'customPrevBtn',
+              nextPageBtn: 'customNextBtn',
+              pages: 'customPages',
+              wrapper: 'customWrapper'
+            }
+          }}
+        />
+        <div style={{
+          margin: '2rem',
+          display: 'flex',
+          justifyContent: 'center'
+        }}>
+          <Button size="lg" className="text-center" variant="primary" onClick={this.handleConsent}>
             Give consent
           </Button>
-        </Container>
+        </div>
       </div>
     );
   }
